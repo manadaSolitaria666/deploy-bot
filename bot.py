6cc46bc1d3198b871d0657ffa63608305ce3f106
manadaSolitaria666 <manadasolitaria666@gmail.com>
2025-09-27 10:05:09 -0600

    Update bot.py
    magy

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ SPREADSHEET_NAME = os.environ.get("SPREADSHEET_NAME", "Candidatos Bot")
 CARRERAS_VALIDAS = [
     "ingeniería", "sistemas", "computación", "computacion", "informática", 
     "informatica", "software", "ciberseguridad", "tecnologías de la información", 
-    "tics", "desarrollo", "Contabilidad", "contabilidad"
+    "tics", "desarrollo"
 ]
 ESPECIALIDADES_VALIDAS = ["ing.software", "ciberseguridad", "desarrollo de software"]
 
@@ -132,7 +132,7 @@ async def recibir_edad(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     """Guarda la edad, la valida y pregunta la experiencia."""
     try:
         edad = int(update.message.text)
-        if edad < 20:
+        if edad < 18:
             await update.message.reply_text("Lo sentimos, debes ser mayor de 20 años para continuar. Proceso finalizado.")
             return ConversationHandler.END
         context.user_data['edad'] = edad
@@ -175,7 +175,7 @@ async def recibir_carrera(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     carrera = update.message.text.lower()
     if any(keyword in carrera for keyword in CARRERAS_VALIDAS):
         context.user_data['carrera'] = update.message.text
-        await update.message.reply_text("Carrera válida. ¿Cuál es tu área de especialidad? (ej. ing. software, ciberseguridad,contabilidad, desarrollo de software)")
+        await update.message.reply_text("Carrera válida. ¿Cuál es tu área de especialidad? (ej. ing. software, ciberseguridad, desarrollo de software)")
         return ESPECIALIDAD
     else:
         await update.message.reply_text("Lo sentimos, buscamos perfiles con carreras relacionadas a la tecnología. Proceso finalizado.")
@@ -284,6 +284,7 @@ def main() -> None:
 if __name__ == "__main__":
 
     main()
+
 
 
 
